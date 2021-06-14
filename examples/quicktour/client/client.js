ebf5d6f1c7256a025621c5e42a092d4f59be4b12
EdAlegrid <ealegrid@17@gmail.com>
2021-06-14 08:56:15 -0400

    updated

--- examples/quicktour/client/client.js
+++ examples/quicktour/client/client.js
@@ -1,47 +1,26 @@
 const m2m = require('m2m');
 
-// create a client application object
 let client = new m2m.Client();
 
-// enter your credentials for server authentication
 client.connect(function(err, result){
-    if(err) return console.error('connect error:', err.message);
+  if(err) return console.error('connect error:', err);
 
-    console.log('result:', result);
+  console.log('result:', result);
 
-    // create a device object from client with access to remote device 100
-    let remoteDevice = client.accessDevice(100); 
+  // create a remote device object from client accessDevice method
+  let device = client.accessDevice(100);
 
-    // using a simple function call data capture
-    remoteDevice.channel('random').getData(function(err, value){
-        if(err) return console.error('getData random error:', err.message);
+  // capture 'random-number' data using a one-time function call
+  device.getData('random-number', function(err, data){
+    if(err) return console.error('getData random-number error:', err.message);
+    console.log('random data', data); // 97
+  });
 
-        console.log('getData random value', value); // 97
-    });
-
-    // using an event-based data capture, data will be pushed   
-    // from the remote device if 'random' data value changes 
-    // 'random' data will be scanned/polled every 5 secs 
-    // by the remote device for any value changes 
-    remoteDevice.channel('random').watch(function(err, value){
-        if(err) return console.error('watch random error:', err.message);
-
-        console.log('watch random value', value); // 97, 101, 115 ... 
-    });
-
-    // Turn ON pin 33 of the remote device 
-    // state value will be true confirming the remote device was ON
-    remoteDevice.output(33).on(function(err, state){
-        if(err) return console.error('remote device output pin 33 ON error:', err.message);
-
-        console.log('remote device output pin 33 ON', state);  // true
-    });
-
-    // Turn OFF pin 33 after 2 secs delay
-    // state value will be false confirming the remote device was OFF
-    remoteDevice.output(33).off(2000, function(err, state){
-        if(err) return console.error('remote device output pin 33 OFF error:', err.message);
-
-        console.log('remote device output pin 33 OFF', state); // false
-    });
-});  
+  // capture 'random-number' data using a push method
+  // the remote device will scan/poll the data every 5 secs (default)
+  // if the value changes, it will push/send the data to the client
+  device.watch('random-number', function(err, data){
+    if(err) return console.error('watch random-number error:', err.message);
+    console.log('watch random data', data); // 81, 68, 115 ...
+  });
+});
