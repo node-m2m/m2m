3c4861abe4b60c135c9c43044385d8b1fc5ba7ab
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-13 14:44:32 -0500

    Add files via upload

--- examples/quicktour/device/device.js
+++ examples/quicktour/device/device.js
@@ -1,19 +1,22 @@
 const m2m = require('m2m');
 
+let testData = 'node-m2m';
+
 // create a device object with a device id of 100
 // this id must must be registered with node-m2m
 let device = new m2m.Device(100);
 
-device.connect(function(err, result){
-  if(err) return console.error('connect error:', err.message);
-
-  console.log('result:', result);
-
-  // set 'random-number' channel as resource  
-  device.setData('random-number', function(err, data){
-    if(err) return console.error('setData random-number error:', err.message);
-
-    let rd = Math.floor(Math.random() * 100);
-    data.send(rd);
+device.connect(() => {
+  // set 'random-number' channel data resource  
+  device.setData('random-number', (data) => {
+    let rn = Math.floor(Math.random() * 100);
+    data.send(rn);
+  });
+  // set 'test-data' channel data resource  
+  device.setData('test-data', (data) => {
+    if(data.payload){
+      testData =  data.payload;
+    }
+    data.send(testData);
   });
 });
