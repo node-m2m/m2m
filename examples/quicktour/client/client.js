18ea5e7d66e5c251d3f84346d8bcc0fce51062d0
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-13 14:43:58 -0500

    Add files via upload

--- examples/quicktour/client/client.js
+++ examples/quicktour/client/client.js
@@ -2,25 +2,29 @@ const m2m = require('m2m');
 
 let client = new m2m.Client();
 
-client.connect(function(err, result){
-  if(err) return console.error('connect error:', err.message);
+client.connect(() => {
 
-  console.log('result:', result);
-
-  // create a remote device object from client accessDevice method
+  // access the remote device using an alias object
   let device = client.accessDevice(100);
 
-  // capture 'random-number' data using a one-time function call
-  device.getData('random-number', function(err, data){
-    if(err) return console.error('getData random-number error:', err.message);
-    console.log('random data', data); // 97
+  // capture 'random-number' data using a pull method
+  device.getData('random-number', (data) => {
+    console.log('getData random-number', data); // 97
   });
 
   // capture 'random-number' data using a push method
-  // the remote device will scan/poll the data every 5 secs (default)
-  // if the value changes, it will push/send the data to the client
-  device.watch('random-number', function(err, data){
-    if(err) return console.error('watch random-number error:', err.message);
-    console.log('watch random data', data); // 81, 68, 115 ...
+  device.watch('random-number', (data) => {
+    console.log('watch random-number', data); // 81, 68, 115 ...
   });
+
+  // update test-data
+  device.sendData('test-data', 'node-m2m is awesome', (data) => {
+    console.log('sendData test-data', data);
+  });
+
+  // capture updated test-data
+  device.getData('test-data', (data) => {
+    console.log('getData test-data', data); // node-m2m is awesome
+  });
+
 });
